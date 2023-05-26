4de2f2ed45ade24f2f9370242c5e199c5b245969
AdilzhanUrynbassarov <190103059@stu.sdu.edu.kz>
2023-05-27 03:33:23 +0600

    First

---
+++ src/main/java/spring/bitlab/sprint_task_1/SprintTask1Application.java
@@ -0,0 +1,13 @@
+package spring.bitlab.sprint_task_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintTask1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintTask1Application.class, args);
+    }
+
+}
